86afe42408471d60d5ada670fac10ff6d5ec25e0
Everton <everton.aaraujo@outlook.com>
2021-03-30 11:26:04 -0300

    Custom tab bar style for iphone

--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
 import Svg, { Path } from 'react-native-svg';
+import { isIphoneX } from 'react-native-iphone-x-helper';
 
 import Home from '../pages/Home';
 
@@ -42,17 +43,41 @@ const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
         </TouchableOpacity>
       </View>
     )
-  } else {
+  } 
+
+  return (
+    <TouchableOpacity 
+      style={{ flex: 1, height: 60, backgroundColor: COLORS.white}}
+      activeOpacity={1}
+      onPress={onPress}
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+const CustomTabBar = (props) => {
+  if (isIphoneX()) {
     return (
-      <TouchableOpacity 
-        style={{ flex: 1, height: 60, backgroundColor: COLORS.white}}
-        activeOpacity={1}
-        onPress={onPress}
-      >
-        {children}
-      </TouchableOpacity>
-    )
+      <View>
+        <View style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: 30,
+          backgroundColor: COLORS.white
+        }}>
+          
+        </View>
+        <BottomTabBar {...props.props} />
+      </View>
+    );
   }
+
+  return (
+    <BottomTabBar {...props.props} />
+  )
 }
 
 const Tabs = () => {
@@ -67,6 +92,11 @@ const Tabs = () => {
           elevation: 0
         }
       }}
+      tabBar={(props) => (
+        <CustomTabBar
+          props={props}
+        />
+      )}
     >
       <Tab.Screen 
         name='Home'
